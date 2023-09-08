601a44710c7e4f6fba1234c912c70bb2a0dcfd4b
Dmytro Mavrosaus <hard359@gmail.com>
2023-09-09 00:27:34 +0300

    ga: deploy to gh-pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import ChatApp from './components/ChatApp';
 
